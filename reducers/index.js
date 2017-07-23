dd09126a280c4e29db1460d24b35c5013ce9a3b3
Panagiwtis Georgiadis <panagiwtis.geor@gmail.com>
2017-07-23 20:58:01 +0100

    Completely removed the Landing Page Reducer and Actions files because they are no longer used.

--- reducers/index.js
+++ reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from "redux";
 
-import LandingPageReducer from "./LandingPageReducer";
 import CurrencyRatesReducer from "./CurrencyRatesReducer";
 
 const rootReducer = combineReducers({
-	LandingPageReducer,
 	CurrencyRatesReducer
 });
 
