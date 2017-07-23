7f38d4cdf5f6fe588aac5fbd75a402f99ff37469
Panagiwtis Georgiadis <panagiwtis.geor@gmail.com>
2017-07-24 00:09:28 +0100

    Added two new cases in the CurrencyRatesReducer in order to separate the "Currency Names" requests from the "Currency Rates" requests.

--- reducers/CurrencyRatesReducer.js
+++ reducers/CurrencyRatesReducer.js
@@ -1,13 +1,14 @@
 import { DRAW_LOADING_IMAGE, HIDE_LOADING_IMAGE,
 		 DRAW_ERROR_MESSAGE, HIDE_ERROR_MESSAGE,
+		 GET_CURRENCY_NAMES_SUCCESS, GET_CURRENCY_NAMES_FAILURE,
 		 GET_CURRENCY_RATES_SUCCESS, GET_CURRENCY_RATES_FAILURE,
-		 DRAW_INITIAL_CURRENCY_RATES 
 		} from "../actions/CurrencyRatesActions";
 
 export default (state = null, action) => {
 
 	var updatedState = Object.assign({}, state);
 
+	updatedState.drawCurrencyTable = false;
 
 	switch(action.type) {
 
@@ -29,22 +30,41 @@ export default (state = null, action) => {
 			updatedState.errorMessage = null;
 			break;
 
-		case GET_CURRENCY_RATES_SUCCESS:
+		case GET_CURRENCY_NAMES_SUCCESS:
 
+			updatedState.showLoadingImage = false;
 			if(action.payload.response.error) {
 
+				updatedState.showErrorMessage = true;
 				updatedState.errorMessage = action.payload.response.error;
-
 			} else {
 
-				updatedState.showLoadingImage = false;
-				updatedState.currencyRates = action.payload.response;
-
 				updatedState.currencyNames = [];
-				for(var props in updatedState.currencyRates.rates) {
+				updatedState.currencyNames.push(action.payload.response.base);
+
+				for(var props in action.payload.response.rates) {
 					updatedState.currencyNames.push(props);
 				}
 				updatedState.selectedCurrency = updatedState.currencyNames[0];
+			}
+			break;
+
+		case GET_CURRENCY_NAMES_FAILURE:
+			updatedState.errorMessage = action.payload.error;
+			updatedState.showLoadingImage = false;
+			break;
+
+		case GET_CURRENCY_RATES_SUCCESS:
+
+			updatedState.showLoadingImage = false;
+			if(action.payload.response.error) {
+
+				updatedState.showErrorMessage = true;
+				updatedState.errorMessage = action.payload.response.error;
+			} else {
+
+				updatedState.currencyRates = action.payload.response;
+				updatedState.drawCurrencyTable = true;
 			}
 			break;
 
